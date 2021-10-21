718519637ff1892dff943deda0c51231a98c0887
natanhp <natanhp@protonmail.com>
2021-10-22 02:09:35 +0700

    Complete all currently hosted item

--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,84 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
+  <v-container fluid grid-list-md>
+    <v-layout row wrap>
+      <v-flex v-for="item in items" :key="item.id" xs12 md6 lg3>
+          <v-card
+            class="mx-auto"
+            max-width="400"
           >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            :src="item.image"
           >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+          </v-img>
+          <v-card-title v-text="item.name"></v-card-title>
+          <v-card-text class="text--primary">
+            <p v-text="item.description"></p>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn
+              color="orange"
+              text
+              :href="item.link"
+		        	target="_blank"
+            >
+              Visit
+            </v-btn>
+
+            <v-btn
+              color="orange"
+              text
+              :href="item.repository_link"
+		        	target="_blank"
+            >
+              Repository
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
+  data () {
+    return {
+      items: [
+        {
+          id: 0,
+          name: "About Me Website",
+          description: "This is a very simple static website for my dicoding submission in Belajar Dasar Pemrograman Web class from Dicoding Academy.",
+          link: "https://natanhp.github.io/About-Me-Website/index.html",
+          repository_link: "https://github.com/natanhp/About-Me-Website",
+          image: "about-me-website.png"
+        },
+        {
+          id: 1,
+          name: "Kouve Petshop Android Application Javadoc",
+          description: "Auto-generated Javadoc of Kouvee Petshop Android Application.",
+          link: "https://natanhp.github.io/Kouve-Petshop-Android-Doc",
+          repository_link: "https://github.com/natanhp/Kouvee-Petshop-Android",
+          image: "kouvee-petshop-android-doc.png"
+        },
+        {
+          id: 2,
+          name: "Kouvee Petshop Frontend",
+          description: "This is the frontend version of Pengembangan Projek Perangkat Lunak's final submission.",
+          link: "https://natanhp.github.io/Kouvee-Petshop-Frontend",
+          repository_link: "https://github.com/natanhp/Kouvee-Petshop-Web-Frond-End",
+          image: "kouvee-petshop-frontend.png"
+        },
+      ],
+    }
+  },
+}
+</script>
